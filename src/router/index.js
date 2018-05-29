8c4a9a98728f9cfbdea91d5d7db7c9d457e9f74c
50048873 <50048873@qq.com>
2018-05-29 11:19:21 +0800

    1.引入Test1.vue测试页面；2.测试了eslint；3为方便后续arcgis测试，暂时关闭了eslint

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Test1 from '@/components/Test1'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Test1',
+      component: Test1
     }
   ]
 })
